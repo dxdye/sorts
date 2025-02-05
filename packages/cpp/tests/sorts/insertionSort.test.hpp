165d2979d625b3885bbabd243a9ee71bf4128b26
dxdye <s-tbertr@haw-landshut.de>
2025-02-05 23:51:47 +0100

    refactor: update tests

--- packages/cpp/tests/sorts/insertionSort.test.hpp
+++ packages/cpp/tests/sorts/insertionSort.test.hpp
@@ -1,8 +1,18 @@
 #include "../../src/sorts/insertionSort.hpp"
 #include "imports.test.hpp"
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_all.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <iostream>
 
 TEST_CASE("Insertion Sort", "[insertionSort]") {
-  Int32Array array = generateArray(10000, 0, 100);
-  Int32Array sortedArray = insertionSort(array);
-  REQUIRE(isSortedCondition(sortedArray));
+  SECTION("Should sort the array") {
+    Int32Array array = generateArray(10000, 0, 100);
+    Int32Array sortedArray = insertionSort(array);
+    BENCHMARK("insertion sort of 10000 elements") {
+      return insertionSort(array);
+    };
+    std::cerr << "Insertion Sort: " << std::endl;
+    REQUIRE(isSortedCondition(sortedArray));
+  }
 }
