9ac91f99aae792b9d3129a1cc294c176171b9408
dxdye <s-tbertr@haw-landshut.de>
2025-02-08 21:58:22 +0100

    refactor: updated inplace merge sort description

--- packages/cpp/tests/sorts/inplaceMergeSort.test.hpp
+++ packages/cpp/tests/sorts/inplaceMergeSort.test.hpp
@@ -6,6 +6,6 @@ TEST_CASE("it should sort the randomly generated array [1M elements]",
           "[inplaceMergeSort]") {
   Int32Array array = generateArray(1000000, 0, 100);
   Int32Array sortedArray = stopWatchWrapper(
-      inplaceMergeSort, "inplace merge sort n log log n: ")(array);
+      inplaceMergeSort, "inplace merge sort log n* (n log n): ")(array);
   REQUIRE(isSortedCondition(sortedArray));
 }
