fa9da16448b0343e22804dd52de569d4c57d0f4e
dxdye <s-tbertr@haw-landshut.de>
2025-02-08 22:08:30 +0100

    refactor: uncomment unused test case

--- packages/cpp/tests/Int32Array/Int32Array.hpp
+++ packages/cpp/tests/Int32Array/Int32Array.hpp
@@ -9,15 +9,16 @@ TEST_CASE("creates Int32Array with 10000 elements", CATEGORY) {
   SECTION("Should have a length of 10000") { CHECK(array.length == 10000); }
   SECTION("Should be sorted") { CHECK(isSortedConditionStrict(array)); }
 }
-// TEST_CASE("utilizes the copy constructor in order to clone an array",
-//           CATEGORY) {
-//   Int32Array array = Int32Array(10000);
-//   for (int i = 0; i < array.length; ++i)
-//     array.set(i, i);
-//   Int32Array clone = Int32Array(array);
-//   SECTION("Should have a length of 10000") { CHECK(clone.length == 10000); }
-//   SECTION("Should be sorted") { CHECK(isSortedConditionStrict(clone)); }
-// }
+
+TEST_CASE("utilizes the copy constructor in order to clone an array",
+          CATEGORY) {
+  Int32Array array = Int32Array(10000);
+  for (int i = 0; i < array.length; ++i)
+    array.set(i, i);
+  Int32Array clone = Int32Array(array);
+  SECTION("Should have a length of 10000") { CHECK(clone.length == 10000); }
+  SECTION("Should be sorted") { CHECK(isSortedConditionStrict(clone)); }
+}
 
 TEST_CASE("uses copy assignement operator to copy array", CATEGORY) {
   Int32Array array = Int32Array(10000);
