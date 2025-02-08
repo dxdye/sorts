0a9578d0f16ee72187c93b1019a20e8fb0af607b
dxdye <s-tbertr@haw-landshut.de>
2025-02-08 20:52:27 +0100

    refactor: add remaining implementation for tests to work

--- packages/cpp/src/sorts/header/inplaceMergeSort.hpp
+++ packages/cpp/src/sorts/header/inplaceMergeSort.hpp
@@ -1,6 +1,7 @@
+#pragma once
 #include "../../helper/header/sortProtoType.hpp"
 #include "../../helper/header/types.hpp"
 
-void merge(Int32Array &arr, i32 start, i32 mid, i32 end);
-void mergeSort(Int32Array &arr, i32 l, i32 r);
+const void merge(Int32Array &arr, i32 start, i32 mid, i32 end);
+const void mergeSort(Int32Array &arr, i32, i32);
 extern const sort inplaceMergeSort;
--- packages/cpp/tests/sorts/inplaceMergeSortN2.test.hpp
+++ packages/cpp/tests/sorts/inplaceMergeSortN2.test.hpp
@@ -1,7 +1,8 @@
 #include "../../src/sorts/header/inplaceMergeSortN2.hpp"
 #include "../imports.test.hpp"
 
-TEST_CASE("it should sort the randomly generated array", "[inplaceMergeSort]") {
+TEST_CASE("it should sort the randomly generated array",
+          "[inplaceMergeSortN2]") {
   Int32Array array = generateArray(10000, 0, 100);
   Int32Array sortedArray = inplaceMergeSortN2(array);
   REQUIRE(isSortedCondition(sortedArray));
